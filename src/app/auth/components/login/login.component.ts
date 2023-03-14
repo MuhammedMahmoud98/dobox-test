a77120142a600e93f023f6ea88f1bb598cf24238
= <=ahmed_hossam_eldeen@outlook.com>
2023-03-14 08:30:23 +0200

    added login form ui

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(private readonly fb: NonNullableFormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+  initForm() {
+    this.loginForm = this.fb.group(this.createFormControls())
+    console.log(this.loginForm);
+  }
+  createFormControls(): { [key: string]: FormControl<string> } {
+    return { email: this.fb.control('', [Validators.required, Validators.email, Validators.minLength(10)]), password: this.fb.control('', [Validators.required, Validators.minLength(6)]) }
+
+  }
+  get formControls() {
+    return this.loginForm.controls;
   }
 
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 }
